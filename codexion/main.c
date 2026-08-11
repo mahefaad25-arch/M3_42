293f1535e39512e763648ff4efc8501296b14c10
bramahefa <bramahef@student.42antananarivo.mg>
2026-08-12 00:11:00 +0300

    add second pars

--- codexion/main.c
+++ codexion/main.c
@@ -3,95 +3,102 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bramahef < bramahef@student.42antananar    +#+  +:+       +#+        */
+/*   By: bramahef <bramahef@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/07 10:02:10 by bramahef          #+#    #+#             */
-/*   Updated: 2026/08/07 12:59:58 by bramahef         ###   ########.fr       */
+/*   Updated: 2026/08/12 00:04:39 by bramahef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <limits.h>
 
-typedef enum {
+typedef enum e_scheduler
+{
     SCHED_FIFO,
     SCHED_EDF
-} t_scheduler;
+}   t_scheduler;
 
-typedef struct s_config {
-    int         number_of_coders;
+typedef struct s_rules
+{
+    int         nb_coders;
     long        time_to_burnout;
     long        time_to_compile;
     long        time_to_debug;
     long        time_to_refactor;
-    int         number_of_compiles_required;
+    int         nb_compiles_req;
     long        dongle_cooldown;
     t_scheduler scheduler;
-} t_config;
+}   t_rules;
 
-// Convertit une chaîne en entier positif avec gestion d'erreurs
-static long parse_positive_long(const char *str, const char *arg_name) {
-    char *endptr;
-    long val = strtol(str, &endptr, 10);
+static long ft_atol(const char *str)
+{
+    long    res;
+    int     i;
 
-    if (*endptr != '\0' || val <= 0 || val > INT_MAX) {
-        fprintf(stderr, "Erreur: '%s' doit être un entier positif valide (%s).\n", arg_name, str);
-        return -1;
+    res = 0;
+    i = 0;
+    if (str[i] == '+')
+        i++;
+    if (!str[i])
+        return (-1);
+    while (str[i])
+    {
+        if (str[i] < '0' || str[i] > '9')
+            return (-1);
+        res = res * 10 + (str[i] - '0');
+        if (res > 2147483647)
+            return (-1);
+        i++;
     }
-    return val;
+    return (res);
 }
 
-int parse_args(int argc, char **argv, t_config *config) {
-    if (argc != 9) {
-        fprintf(stderr, "Usage: %s number_of_coders time_to_burnout time_to_compile "
-                        "time_to_debug time_to_refactor number_of_compiles_required "
-                        "dongle_cooldown scheduler\n", argv[0]);
-        return 0;
-    }
+int init_rules(int argc, char **argv, t_rules *rules)
+{
+    if (argc != 9)
+        return (1);
 
-    config->number_of_coders = (int)parse_positive_long(argv[1], "number_of_coders");
-    config->time_to_burnout = parse_positive_long(argv[2], "time_to_burnout");
-    config->time_to_compile = parse_positive_long(argv[3], "time_to_compile");
-    config->time_to_debug = parse_positive_long(argv[4], "time_to_debug");
-    config->time_to_refactor = parse_positive_long(argv[5], "time_to_refactor");
-    config->number_of_compiles_required = (int)parse_positive_long(argv[6], "number_of_compiles_required");
-    config->dongle_cooldown = parse_positive_long(argv[7], "dongle_cooldown");
+    rules->nb_coders = (int)ft_atol(argv[1]);
+    rules->time_to_burnout = ft_atol(argv[2]);
+    rules->time_to_compile = ft_atol(argv[3]);
+    rules->time_to_debug = ft_atol(argv[4]);
+    rules->time_to_refactor = ft_atol(argv[5]);
+    rules->nb_compiles_req = (int)ft_atol(argv[6]);
+    rules->dongle_cooldown = ft_atol(argv[7]);
+    if (rules->nb_coders <= 0 || rules->time_to_burnout <= 0 ||
+        rules->time_to_compile <= 0 || rules->time_to_debug <= 0 ||
+        rules->time_to_refactor <= 0 || rules->nb_compiles_req < 0 ||
+        rules->dongle_cooldown < 0)
+        return (1);
+    if (strcmp(argv[8], "fifo") == 0)
+        rules->scheduler = SCHED_FIFO;
+    else if (strcmp(argv[8], "edf") == 0)
+        rules->scheduler = SCHED_EDF;
+    else
+        return (1);
 
-    // Vérification de la validité de tous les entiers
-    if (config->number_of_coders < 0 || config->time_to_burnout < 0 ||
-        config->time_to_compile < 0 || config->time_to_debug < 0 ||
-        config->time_to_refactor < 0 || config->number_of_compiles_required < 0 ||
-        config->dongle_cooldown < 0) {
-        return 0;
-    }
-
-    // Parsing du scheduler (fifo ou edf)
-    if (strcmp(argv[8], "fifo") == 0) {
-        config->scheduler = SCHED_FIFO;
-    } else if (strcmp(argv[8], "edf") == 0) {
-        config->scheduler = SCHED_EDF;
-    } else {
-        fprintf(stderr, "Erreur: scheduler doit être 'fifo' ou 'edf' (reçu: '%s').\n", argv[8]);
-        return 0;
-    }
-
-    return 1;
+    return (0);
 }
 
-int main(int argc, char **argv) {
-    t_config config;
+int main(int argc, char *argv[])
+{
+    t_rules rules;
 
-    if (!parse_args(argc, argv, &config)) {
-        return EXIT_FAILURE;
+    if (init_rules(argc, argv, &rules) != 0)
+    {
+        printf("Error: Invalid arguments.\n");
+        return (1);
     }
 
-    // Exemple de confirmation du parsing
-    printf("Parsing réussi :\n");
-    printf(" - Coders: %d\n", config.number_of_coders);
-    printf(" - Burnout: %ld ms\n", config.time_to_burnout);
-    printf(" - Scheduler: %s\n", config.scheduler == SCHED_FIFO ? "fifo" : "edf");
+    printf("--- Configuration chargée avec succès ---\n");
+    printf("Coders: %d | Burnout: %ldms | Compile: %ldms\n",
+           rules.nb_coders, rules.time_to_burnout, rules.time_to_compile);
+    printf("Debug: %ldms | Refactor: %ldms | Compiles req: %d\n",
+           rules.time_to_debug, rules.time_to_refactor, rules.nb_compiles_req);
+    printf("Cooldown: %ldms | Scheduler: %s\n",
+           rules.dongle_cooldown, rules.scheduler == SCHED_FIFO ? "FIFO" : "EDF");
 
-    return EXIT_SUCCESS;
+    return (0);
 }
