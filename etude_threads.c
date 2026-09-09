ad3304aac02462b4cbb7fe65c9b9ef9d50a554be
Bernard Ramahefason <bramahef@c1r6s1.42antananarivo.mg>
2026-09-09 18:46:54 +0300

    presque terminer avec ia 2

--- etude_threads.c
+++ etude_threads.c
@@ -6,18 +6,17 @@
 /*   By: bramahef < bramahef@student.42antananar    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/09/07 06:17:09 by bramahef          #+#    #+#             */
-/*   Updated: 2026/09/07 06:17:14 by bramahef         ###   ########.fr       */
+/*   Updated: 2026/09/07 06:25:25 by bramahef         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pthread.h>
 #include <stdio.h>
 
-unsigned int	counter = 0;
-pthread_mutex_t	lock;
-
-void	*counter_routine(void *arg)
+void	*counter_routine(int counter)
 {
+	pthread_mutex_t	lock;
+
 	for (int i = 0; i < 100000; i++)
 	{
 		pthread_mutex_lock(&lock); // Début de section critique
@@ -30,11 +29,15 @@ void	*counter_routine(void *arg)
 int	main(void)
 {
 	pthread_t t1, t2;
+	pthread_mutex_t lock;
+	int count;
+	int counter;
+	count = 0;
 
 	pthread_mutex_init(&lock, NULL);
 
-	pthread_create(&t1, NULL, counter_routine, NULL);
-	pthread_create(&t2, NULL, counter_routine, NULL);
+	counter = pthread_create(&t1, NULL, counter_routine(count), NULL);
+	counter = pthread_create(&t2, NULL, counter_routine(count), NULL);
 
 	pthread_join(t1, NULL);
 	pthread_join(t2, NULL);
